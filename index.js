83385906fa7de04c992e9cc7a8b9165d760f6230
Amy Shafer <amy.u.shafer@gmail.com>
2022-03-04 17:15:19 -0500

    created a file for prompt questions

--- index.js
+++ index.js
@@ -3,67 +3,28 @@ const Employee = require("./team/Employee");
 const Engineer = require("./team/Engineer");
 const Intern = require("./team/Intern");
 const Manager = require("./team/Manager");
+const questions = require("./teamQuestions");
 
 const team = [];
 
-// questions for engineer class
-const engineerQuestions = [{
-    type: 'input',
-    message: 'What is the engineer\'s name?',
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: 'What is the engineer\'s id?',
-    name: 'id',
-  },
-  {
-    type: 'input',
-    message: 'What is the engineer\'s email?',
-    name: 'email',
-  },
-  {
-    type: 'input',
-    message: 'What is the engineer\'s GitHub user name?',
-    name: 'github',
-  }
-]
-// questions for intern class
-const internQuestions = [{
-    type: 'input',
-    message: 'What is the intern\'s name?',
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: 'What is the intern\'s id?',
-    name: 'id',
-  },
-  {
-    type: 'input',
-    message: 'What is the intern\'s email?',
-    name: 'email',
-  },
-  {
-    type: 'input',
-    message: 'What is the intern\'s school?',
-    name: 'school',
-  }
-]
+function addManager(userInput) {
+  return inquirer.prompt(userInput);
+//     }
+//     manager = new Manager();
+//     team.push(manager);
+//     manager.setName(answers.name);
+//   }
+};
 
 function addEngineer(userInput) {
-  
-    return inquirer.prompt(userInput);
-  
+  return inquirer.prompt(userInput);
   // engineer = new Engineer();
   // team.push(engineer);
   // engineer.setName(answers.name);
 };
 
 function addIntern(userInput) {
-  
-    return inquirer.prompt(userInput);
-  
+  return inquirer.prompt(userInput);
   // intern = new Intern();
   // team.push(intern);
   // intern.setName(answers.name);
@@ -72,48 +33,21 @@ function addIntern(userInput) {
 // Pull data from the user inputs to dynamically create a team of employees
 const generateTeam = () => {
   console.log("Welcome to New Horizons, the first team to explore Pluto up close via spacecraft. Join us as we continue our journey into the solar system's coldest, darkest frontiers. \n Assemble your team:");
-  return inquirer.prompt([{
-        type: 'input',
-        message: 'What is the team manager\'s name?',
-        name: 'name',
-      },
-      {
-        type: 'input',
-        message: 'What is the team manager\'s id?',
-        name: 'id',
-      },
-      {
-        type: 'input',
-        message: 'What is the team manager\'s email?',
-        name: 'email',
-      },
-      {
-        type: 'list',
-        message: 'Which type of team member would you like to add?',
-        name: 'team',
-        choices: [{
-            name: 'Engineer',
-          },
-          {
-            name: 'Intern',
-          },
-        ],
-      },
-    ])
-    .then(answers => {
-      if (answers.team.choices === 'Engineer') {
-          addEngineer(engineerQuestions);
-      } else if (answers.team.choices === 'Intern') {
-          addIntern(internQuestions); 
-      } else {
-        const addManager = () => {
-          return inquirer.prompt(answers)
-        }
-        manager = new Manager();
-        team.push(manager);
-        manager.setName(answers.name);
-      }
-    });
+  return new Manager(addManager(managerQuestions)
+  
+  .then(answers => {
+    team.push(answers);
+    const test = JSON.stringify(team);
+    console.log(test);
+    if (answers.team === 'Engineer') {
+      engineer = new Engineer(addEngineer(engineerQuestions));
+      team.push(engineer);
+      console.log("TEAM:" + team);
+    } else if (answers.team === 'Intern') {   
+      intern = new Intern(addIntern(internQuestions));
+      team.push(intern);
+    } 
+  }))
 }
 
 generateTeam();
---
+++ teamQuestions.js
@@ -0,0 +1,85 @@
+
+// questions for manager class
+const managerQuestions = [
+  {
+    type: 'input',
+    message: 'What is the team manager\'s name?',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'What is the team manager\'s id?',
+    name: 'id',
+  },
+  {
+    type: 'input',
+    message: 'What is the team manager\'s email?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What is the team manager\'s office number?',
+    name: 'officeNumber',
+  },
+  {
+    type: 'list',
+    message: 'Which type of team member would you like to add?',
+    name: 'team',
+    choices: [
+      {
+        name: 'Engineer',
+      },
+      {
+        name: 'Intern',
+      },
+    ],
+}];
+  
+// questions for engineer class
+const engineerQuestions = [
+  {
+    type: 'input',
+    message: 'What is the engineer\'s name?',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'What is the engineer\'s id?',
+    name: 'id',
+  },
+  {
+    type: 'input',
+    message: 'What is the engineer\'s email?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What is the engineer\'s GitHub user name?',
+    name: 'github',
+  }
+]
+// questions for intern class
+const internQuestions = [
+  {
+    type: 'input',
+    message: 'What is the intern\'s name?',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'What is the intern\'s id?',
+    name: 'id',
+  },
+  {
+    type: 'input',
+    message: 'What is the intern\'s email?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What is the intern\'s school?',
+    name: 'school',
+  }
+];
+  
+module.exports = managerQuestions, engineerQuestions, internQuestions;
